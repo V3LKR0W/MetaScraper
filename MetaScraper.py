8d01513e34c9e3799049bab651e274d894800b86
v3lkr0w <r3v3rm@protonmail.com>
2020-12-05 11:57:01 -0500

    adding flask

--- MetaScraper.py
+++ MetaScraper.py
@@ -1,6 +1,9 @@
+from flask import Flask
 import requests
 from bs4 import BeautifulSoup as bs
 
+
+app = Flask(__name__)
 
 
 class MetaScraper():
@@ -23,9 +26,19 @@ class MetaScraper():
             Response = {
                 'preview_description':str(v['content'])
             }
-            print(Response)
+            return Response
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Nothing at the moment.'
+
+
+@app.route('/preview/<url>', methods=['GET'])
+def preview(url):
+    return MetaScraper.get_preview(f'https://{url}', useragent=None)
 
 
 
-#MetaScraper.get_preview('https://reddit.com', useragent=None)
-
+if __name__ == '__main__':
+    app.run()
