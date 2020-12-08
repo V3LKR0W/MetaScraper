d6de116e37ac4b77ad94be1d3063ddbb96db7cdd
v3lkr0w <r3v3rm@protonmail.com>
2020-12-07 21:17:12 -0500

    added more meta methods and page routes

--- MetaScraper.py
+++ MetaScraper.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, request
-import requests
+from flask import Flask, jsonify, request, render_template
+import requests, base64, random, string
 from bs4 import BeautifulSoup as bs
 
 
@@ -11,10 +11,13 @@ class MetaScraper():
         self.url = url
         self.useragent = useragent
         
+    error = {
+        'Missing'
+    }
     def get_preview(url, useragent):
         if useragent == None or '':
             usr_agent = {
-                'User-Agent':'HTTP/MetaCrawler/DEFAULT'
+                'User-Agent':'HTTP/MetaScraper/DEFAULT'
             }
         else:
             usr_agent = {
@@ -27,39 +30,69 @@ class MetaScraper():
                 'pageURL': '',
                 'pageTitle': '',
                 'pageDescription': '',
+                'themeColor': '',
                 'pageOG:image': '',
+                'pageOG:video': '',
             }
             
             Response['pageURL'] = str(url)
             
             for v in soup.find_all('title'):
                 if v.text == '':
-                    Response['pageTitle'] = 'N/A'
+                    Response['pageTitle'] = error[0]
                 else:
                     Response['pageTitle'] = v.text  
                 
             for v in soup.find_all('meta',{'name':'description'}):
                 if v.content == '':
-                    Response['pageDescription'] = 'N/A'
+                    Response['pageDescription'] = error[0]
                 else:
                     Response['pageDescription'] = v['content']
+              
+            for v in soup.find_all('meta',{'name':'theme-color'}):
+                if v.content == '':
+                    Response['themeColor'] = error[0]
+                else:
+                    Response['themeColor'] = v['content']  
                     
             for v in soup.find_all('meta',{'property':'og:image'}):
                 if v.content == '':
-                    Response['pageOG:image'] = 'N/A'
+                    Response['pageOG:image'] = error[0]
                 else:
                     Response['pageOG:image'] = v['content']
                 
+            for v in soup.find_all('meta', {'property':'og:video:url'}):
+                if v.content == '':
+                    Response['pageOG:video'] = error[0]
+                else:
+                    Response['pageOG:video'] = v['content']
                 
-        except Exception:
+        except Exception as e:
             return jsonify({'Error':'Could not establish a connection to target domain'})
         
         return Response
 
 
+def createKey():
+    s = string.ascii_letters
+    key = ''.join(random.choice(s) for length in range(20))
+    return key
+
+
 @app.route('/', methods=['GET'])
 def index():
-    return 'Nothing at the moment.'
+    return render_template('index.html')
+
+
+@app.route('/keyGen', methods=['POST', 'GET'])
+def keyGen():
+    if request.method == 'POST':
+        encode = bytes(createKey(), 'utf-8')
+        keygen = base64.b64encode(encode, altchars=None)
+        return jsonify({'API_KEY':str(keygen)})
+    else:
+        return render_template('keygen.html')
+
 
 
 @app.route('/preview', methods=['GET'])
@@ -68,7 +101,7 @@ def preview():
     if url == '':
         return jsonify({'Error':'No target specified'})
     else:
-        return MetaScraper.get_preview(url, useragent=None)
+        return MetaScraper.get_preview(url, useragent='MetaScraper/PagePreviewer')
 
 
 
